2d52b1a0d2896b3d66aaf9d17cf0bd64add9948a
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-01 21:52:05 -0300

    Created the service HybridRegistrationServiceImpl

---
+++ v3/src/main/kotlin/com/api/v3/cars/services/HybridRegistrationServiceImpl.kt
@@ -0,0 +1,21 @@
+package com.api.v3.cars.services
+
+import com.api.v3.cars.domain.Hybrid
+import com.api.v3.cars.dtos.CarRegistrationRequestDto
+import com.api.v3.cars.dtos.CarResponseDto
+import com.api.v3.cars.utils.CarResponseMapper
+import jakarta.validation.Valid
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class HybridRegistrationServiceImpl: CarRegistrationService() {
+
+    override suspend fun register(requestDto: @Valid CarRegistrationRequestDto): CarResponseDto {
+        return withContext(Dispatchers.IO) {
+            val hybrid = Hybrid.of(requestDto)
+            val savedHybrid = carRepository.save(hybrid)
+            CarResponseMapper.map(savedHybrid)
+        }
+    }
+
+}
