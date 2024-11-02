c9b49de3d16d22ad1801cb60a6b4af49f112b0ff
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-01 22:44:04 -0300

    Created the util CarFinder

---
+++ v3/src/main/kotlin/com/api/v3/cars/utils/CarFinder.kt
@@ -0,0 +1,30 @@
+package com.api.v3.cars.utils
+
+import com.api.v3.cars.domain.Car
+import com.api.v3.cars.domain.CarRepository
+import com.api.v3.cars.exceptions.CarNotFoundException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.singleOrNull
+import kotlinx.coroutines.withContext
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class CarFinder {
+
+    @Autowired
+    private lateinit var carRepository: CarRepository
+
+    suspend fun find(vin: String): Car {
+        return withContext(Dispatchers.IO) {
+            val existingCar = carRepository
+                .findAll()
+                .filter { e -> e.vin == vin }
+                .singleOrNull()
+            if (existingCar == null) throw CarNotFoundException()
+            existingCar
+        }
+    }
+
+}
