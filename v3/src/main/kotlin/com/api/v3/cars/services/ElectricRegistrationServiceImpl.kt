272e9aff0037fc79c3e6f1495cd69689caffe165
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-01 21:53:43 -0300

    Created the service ElectricRegistrationServiceImpl

---
+++ v3/src/main/kotlin/com/api/v3/cars/services/ElectricRegistrationServiceImpl.kt
@@ -0,0 +1,21 @@
+package com.api.v3.cars.services
+
+import com.api.v3.cars.domain.Electric
+import com.api.v3.cars.dtos.CarRegistrationRequestDto
+import com.api.v3.cars.dtos.CarResponseDto
+import com.api.v3.cars.utils.CarResponseMapper
+import jakarta.validation.Valid
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ElectricRegistrationServiceImpl: CarRegistrationService() {
+
+    override suspend fun register(requestDto: @Valid CarRegistrationRequestDto): CarResponseDto {
+        return withContext(Dispatchers.IO) {
+            val electric = Electric.of(requestDto)
+            val savedElectric = carRepository.save(electric)
+            CarResponseMapper.map(savedElectric)
+        }
+    }
+
+}
