b39204923adc34fa030abbd3c6d910eeb842a278
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-01 21:55:09 -0300

    Created the service CrossoverRegistrationServiceImpl

---
+++ v3/src/main/kotlin/com/api/v3/cars/services/CrossoverRegistrationServiceImpl.kt
@@ -0,0 +1,21 @@
+package com.api.v3.cars.services
+
+import com.api.v3.cars.domain.Crossover
+import com.api.v3.cars.dtos.CarRegistrationRequestDto
+import com.api.v3.cars.dtos.CarResponseDto
+import com.api.v3.cars.utils.CarResponseMapper
+import jakarta.validation.Valid
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class CrossoverRegistrationServiceImpl: CarRegistrationService() {
+
+    override suspend fun register(requestDto: @Valid CarRegistrationRequestDto): CarResponseDto {
+        return withContext(Dispatchers.IO) {
+            val crossover = Crossover.of(requestDto)
+            val savedCrossover = carRepository.save(crossover)
+            CarResponseMapper.map(savedCrossover)
+        }
+    }
+
+}
