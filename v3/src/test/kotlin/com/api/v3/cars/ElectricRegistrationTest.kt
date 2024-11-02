272c815c3d5aedac04b11150f582eddcc2497634
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-02 02:35:49 -0300

    Created the test class ElectricRegistrationTest

---
+++ v3/src/test/kotlin/com/api/v3/cars/ElectricRegistrationTest.kt
@@ -0,0 +1,32 @@
+package com.api.v3.cars
+
+import com.api.v3.vehicles.dtos.VehicleResponseDto
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+internal class ElectricRegistrationTest {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    val requestDto = VehicleResponseDto(
+        "Acadia",
+        "GMC",
+        20_000.0,
+        2024
+    )
+
+    @Test
+    fun testSuccessfulRegistration() {
+        webTestClient
+            .post()
+            .uri("api/v3/cars/electric")
+            .bodyValue(requestDto)
+            .exchange()
+            .expectStatus().is2xxSuccessful()
+    }
+
+}
