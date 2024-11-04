e5537e72c93e772a67c9aaac44e2b67aa82748eb
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-04 15:33:04 -0300

    Created the util PurchaseOrderNumberGenerator

--- v3/src/main/kotlin/com/api/v3/purchases/domain/Purchase.kt
+++ v3/src/main/kotlin/com/api/v3/purchases/domain/Purchase.kt
@@ -1,16 +1,19 @@
-package com.api.v3.purchases
+package com.api.v3.purchases.domain
 
 import com.api.v3.cars.domain.superclass.Car
 import com.api.v3.customers.domain.Customer
 import com.api.v3.employees.domain.superclass.Employee
+import com.api.v3.purchases.utils.PurchaseOrderNumberGenerator
 import com.api.v3.utils.Constants
 import org.springframework.data.annotation.Id
+import java.math.BigInteger
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.UUID
 
 data class Purchase(
     @Id var id: UUID,
+    var orderNumber: BigInteger,
     var customer: Customer,
     var salesperson: Employee,
     var car: Car,
@@ -24,6 +27,7 @@ data class Purchase(
         fun of(customer: Customer, salesperson: Employee, car: Car, discount: Double): Purchase {
             return Purchase(
                 UUID.randomUUID(),
+                PurchaseOrderNumberGenerator.generate(),
                 customer,
                 salesperson,
                 car,
