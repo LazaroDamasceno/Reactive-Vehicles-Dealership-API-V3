4e8d6dd7f20b3ee2c25d6ace795524fdc31c8f85
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-04 15:53:50 -0300

    Renamed CarFinder as CarFinderUtil

--- v3/src/main/kotlin/com/api/v3/cars/utils/CarFinderUtil.kt
+++ v3/src/main/kotlin/com/api/v3/cars/utils/CarFinderUtil.kt
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class CarFinder {
+class CarFinderUtil {
 
     @Autowired
     private lateinit var carRepository: CarRepository
