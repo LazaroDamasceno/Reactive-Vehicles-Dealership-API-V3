08611e6e11a1c04beead1ec3ac41dbd7e373de56
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-04 15:57:21 -0300

    Created the controller PurchaseController

---
+++ v3/src/main/kotlin/com/api/v3/purchases/controllers/PurchaseController.kt
@@ -0,0 +1,24 @@
+package com.api.v3.purchases.controllers
+
+import com.api.v3.purchases.dtos.PurchaseRegistrationRequestDto
+import com.api.v3.purchases.services.PurchaseRegistrationService
+import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v3/purchases")
+class PurchaseController {
+
+    @Autowired
+    private lateinit var registrationService: PurchaseRegistrationService
+
+    @PostMapping
+    suspend fun register(@RequestBody requestDto: @Valid PurchaseRegistrationRequestDto) {
+        return registrationService.register(requestDto)
+    }
+
+}
