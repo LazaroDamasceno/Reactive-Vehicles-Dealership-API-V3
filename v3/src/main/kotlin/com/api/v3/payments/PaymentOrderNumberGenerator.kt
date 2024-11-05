b00b2606cc127a5f32c4977c347f473efdde46c3
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-04 23:48:25 -0300

    Created the util PaymentOrderNumberGenerator

---
+++ v3/src/main/kotlin/com/api/v3/payments/PaymentOrderNumberGenerator.kt
@@ -0,0 +1,26 @@
+package com.api.v3.payments
+
+import java.math.BigInteger
+import java.time.LocalDateTime
+
+class PaymentOrderNumberGenerator {
+
+    companion object {
+
+        private val year = LocalDateTime.now().year
+        private val month = LocalDateTime.now().month
+        private val day = LocalDateTime.now().dayOfMonth
+        private val hour = LocalDateTime.now().hour
+        private val minute = LocalDateTime.now().minute
+        private val second = LocalDateTime.now().second
+        private val text = "$year$month$day-$hour$minute$second-0000000"
+        private var biText = BigInteger(text)
+
+        fun generate(): BigInteger {
+            biText = biText.add(BigInteger.ONE)
+            return biText
+        }
+
+    }
+
+}
