3337b0d2f721dfd9bac7f80cfc5362b65b350447
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-04 21:38:19 -0300

    Created the test class DebitCardRegistrationTest

---
+++ v3/src/test/kotlin/com/api/v3/cards/DebitCardRegistrationTest.kt
@@ -0,0 +1,30 @@
+package com.api.v3.cards
+
+import com.api.v3.cards.dtos.CardOwnerDataDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.Test
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+internal class DebitCardRegistrationTest {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    val requestDto = CardOwnerDataDto(
+        "John Doe",
+        "123456789"
+    )
+
+    @Test
+    fun testRegistration() {
+        webTestClient
+            .post()
+            .uri("api/v3/cards/debit-card")
+            .bodyValue(requestDto)
+            .exchange()
+            .expectStatus().is2xxSuccessful()
+    }
+
+}
