6e16da40a1663f9c2f2f0ba385577e4ed9275b2d
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-05 00:06:00 -0300

    Added the fields paymentResponseDto (PaymentResponseDto) and createdAt (ZonedDateTime) to PurchaseResponseDto

--- v3/src/main/kotlin/com/api/v3/purchases/dtos/PurchaseResponseDto.kt
+++ v3/src/main/kotlin/com/api/v3/purchases/dtos/PurchaseResponseDto.kt
@@ -3,7 +3,9 @@ package com.api.v3.purchases.dtos
 import com.api.v3.cars.dtos.CarResponseDto
 import com.api.v3.customers.dtos.CustomerResponseDto
 import com.api.v3.employees.dtos.EmployeeResponseDto
+import com.api.v3.payments.dtos.PaymentResponseDto
 import java.math.BigInteger
+import java.time.ZonedDateTime
 
 data class PurchaseResponseDto(
     val orderNumber: BigInteger,
@@ -11,5 +13,7 @@ data class PurchaseResponseDto(
     val salespersonResponseDto: EmployeeResponseDto,
     val carResponseDto: CarResponseDto,
     val discount: Double,
-    val finalPrice: Double
+    val finalPrice: Double,
+    val paymentResponseDto: PaymentResponseDto,
+    val createdAt: ZonedDateTime
 )
