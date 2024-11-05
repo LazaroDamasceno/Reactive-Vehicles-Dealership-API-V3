8b7d489f7242bc3dddad6c42723c9d127fe0c901
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-04 22:31:12 -0300

    Changed CardDeletionTest

--- v3/src/test/kotlin/com/api/v3/cards/CardDeletionTest.kt
+++ v3/src/test/kotlin/com/api/v3/cards/CardDeletionTest.kt
@@ -1,10 +1,14 @@
 package com.api.v3.cards
 
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CardDeletionTest {
 
@@ -12,37 +16,41 @@ class CardDeletionTest {
     private lateinit var webTestClient: WebTestClient
 
     @Test
+    @Order(1)
     fun testSuccessfulDeletion1() {
         webTestClient
             .delete()
-            .uri("api/v3/cards/9950427275954341")
+            .uri("api/v3/cards/0243723997710800")
             .exchange()
             .expectStatus().is2xxSuccessful()
     }
 
     @Test
+    @Order(2)
     fun testUnsuccessfulDeletion1() {
         webTestClient
             .delete()
-            .uri("api/v3/cards/9950427275954341")
+            .uri("api/v3/cards/0243723997710800")
             .exchange()
             .expectStatus().is5xxServerError()
     }
 
     @Test
+    @Order(3)
     fun testSuccessfulDeletion2() {
         webTestClient
             .delete()
-            .uri("api/v3/cards/7597375365339377")
+            .uri("api/v3/cards/6579320614964825")
             .exchange()
             .expectStatus().is2xxSuccessful()
     }
 
+    @Order(4)
     @Test
     fun testUnsuccessfulDeletion2() {
         webTestClient
             .delete()
-            .uri("api/v3/cards/7597375365339377")
+            .uri("api/v3/cards/6579320614964825")
             .exchange()
             .expectStatus().is5xxServerError()
     }
